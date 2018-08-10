2cf7d5e66150ed8e3c55e8904de06d9b3ca189b2
johnny <johnnylab@163.com>
2018-08-10 14:30:08 +0800

    初始化SpringBoot项目

---
+++ src/main/java/tech/johnnylab/antdemo/AntDemoApplication.java
@@ -0,0 +1,12 @@
+package tech.johnnylab.antdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/tech/johnnylab/antdemo/HelloController.java
@@ -0,0 +1,13 @@
+package tech.johnnylab.antdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return "hello,Spring Boot";
+    }
+}
